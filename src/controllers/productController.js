0fd2a9ffd0fa51ba7fa613d7a885c119c631a62c
amuslera <arielmuslera@gmail.com>
2022-05-08 18:19:06 -0700

    validacion express validator para products a medio hacer

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,12 +1,11 @@
 const db = require("../../models");
 const fs = require("fs");
 const path = require("path");
+const { validationResult } = require("express-validator");
+
 
 // Controllers
 const productController = {
-  // list: (req, res) => {
-
-  // },
   list: (req, res) => {
     db.Products.findAll({
       include: [
@@ -16,7 +15,6 @@ const productController = {
         { association: "imagenes" },
       ],
     }).then(function (products) {
-      // res.send(products[0].imagenes[0].nombreArchivo);
       res.render("productList", {
         title: "Todos los productos",
         products: products,
@@ -38,25 +36,32 @@ const productController = {
   },
 
   store: async (req, res) => {
-    let productoNuevo = {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render("productCreate", {errors: errors.mapped()});
+    }
+    else {
+      let productoNuevo = {
       nombre: req.body.productName,
       descripcion: req.body.productDescription,
       precio: req.body.productPrice,
       id_color: req.body.color,
       id_talle: req.body.talle,
-    };
-    let x = await db.Products.create(productoNuevo);
-    let idP = x.dataValues.id;
-    for (let i = 0; i < req.files.length; i++) {
-      let objeto = { id_Producto: idP, nombreArchivo: req.files[i].filename };
-      await db.imagenProducto.create(objeto);
+      };
+      let x = await db.Products.create(productoNuevo);
+      let idP = x.dataValues.id;
+      for (let i = 0; i < req.files.length; i++) {
+        let objeto = { id_Producto: idP, nombreArchivo: req.files[i].filename };
+        await db.imagenProducto.create(objeto);
+      }
+      for (let i = 0; i < req.body.categorias.length; i++) {
+        let objeto2 = { id_Producto: idP, id_Categoria: req.body.categorias[i] };
+        await db.Producto_Categoria.create(objeto2);
+      }
+      res.redirect("/products");
     }
-    for (let i = 0; i < req.body.categorias.length; i++) {
-      let objeto2 = { id_Producto: idP, id_Categoria: req.body.categorias[i] };
-      await db.Producto_Categoria.create(objeto2);
-    }
-    res.redirect("/products");
   },
+  
   detail: (req, res) => {
     db.Products.findByPk(req.params.id, {
       include: [
