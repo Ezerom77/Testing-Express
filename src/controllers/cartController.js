1092b6d04a359694f4cbfde0da897ed07596f197
Ezerom77 <fullstack@ezerom.com.ar>
2022-05-10 17:43:48 -0300

    Iniciando el carrito

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,19 +1,25 @@
 const db = require("../../models");
 
 const cartController = {
-  index: (req,res) => {
-      res.render('cartDetail',{
-          title: 'Carrito de compras',
-      })
+  index: (req, res) => {
+    res.send("todavia no esta definida esta vista")
   },
-  add: (req,res) => {
+  add: (req, res) => {
     let cart = req.session.cart;
     let prod = req.params.id;
-    console.log(cart,prod)
-    db.Products.findByPk(prod).then(function(product){
-        cart.push(product);
-        req.session.cart = cart;
-        res.render('cartDetail',{cart: cart});
-    })}
-  }
-    module.exports = cartController;
+    console.log(cart, prod);
+    db.Products.findByPk(prod, {
+      include: [
+        { association: "color" },
+        { association: "talle" },
+        { association: "categorias" },
+        { association: "imagenes" },
+      ],
+    }).then(function (product) {
+      cart.push(product);
+      req.session.cart = cart;
+      res.render("cartDetail", { cart: cart });
+    });
+  },
+};
+module.exports = cartController;
