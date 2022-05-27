6396f43ea033d650edb6ec870f85259cbbab4520
Ezerom77 <fullstack@ezerom.com.ar>
2022-05-27 20:42:22 -0300

    productos por categoria

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -131,10 +131,25 @@ const apiController = {
           fecha_creacion: user.fecha_creacion,
           ultima_edicion: user.ultima_edicion,
           fecha_baja: user.fecha_baja,
-        }
+        },
       });
     });
-  }
+  },
+  test: (req, res) => {
+    db.Producto_Categoria.findAll({
+      include: [{ association: "categorias" }],
+      group: ['id_Categoria'],
+      attributes: [[sequelize.fn('COUNT', 'id_Producto'), 'sumaProductos']],
+    }).then((producto_categoria) => {
+      console.log("primeravez"+producto_categoria[0].sumaProductos);
+      res.status(200).json({
+        status: 200,
+        message: "ProductosPorCategoria",
+        url: "api/test",
+        producto_categoria: producto_categoria,
+      });
+    });
+  },
 };
 
 module.exports = apiController;
